9cb7819d6d0ccb6db8a3d286cb917eb04cadec42
de-Paul-Rwa250 <52076676+de-Paul-Rwa250@users.noreply.github.com>
2025-11-22 20:54:02 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -92,18 +92,18 @@ const products = [
     },
     {
         id: "P008",
-        name: "Fitbit Charge 5 Fitness Tracker",
+        name: "Dell 24-inch all-in-one computer EC-24250",
         images: [
             "images/P008/image1.jpg"
             "images/P008/image2.jpg"
             "images/P008/image3.jpg"
-            "images/P008/image4.jpg"
+            "images/P008/iimage4.jpg"
             "images/P008/image5.jpg"
         ],
         thumbnail: "images/P008/image1.jpg",
         prices: {
-            kigali: "1,999,000 RF",
-            muhanga: "1,899,000 RF"
+            kigali: "1,499,000 RF",
+            muhanga: "1,599,000 RF"
         }
     }
 ];
