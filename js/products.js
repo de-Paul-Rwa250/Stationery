5fc76585748f73a650885cc1e1fc9b3da9e5d3a8
de-Paul-Rwa250 <52076676+de-Paul-Rwa250@users.noreply.github.com>
2025-11-24 08:44:01 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -95,12 +95,14 @@ const products = [
         name: "Dell 24-inch all-in-one computer EC-24250",
         images: [
             "images/P008/image1.jpg",
-             "images/P008/image2.jpg"
+            "images/P008/image2.jpg",
+            "images/P008/image3.jpg",
+            "images/P008/image5.jpg"
         ],
         thumbnail: "images/P008/image1.jpg",
         prices: {
-            kigali: "153,000 RF",
-            muhanga: "173,400 RF"
+            kigali: "1,500,000 RF",
+            muhanga: "1,500,000 RF"
         }
     }
 ];
